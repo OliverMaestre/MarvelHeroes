17122c41f3bbd067d6fec3a6cbd3af903129d37b
OliverMaestre <omaestre86@gmail.com>
2021-06-09 12:55:53 +0200

    Change test to local

--- app/src/test/java/com/omaestre/marvel/injection/RepositoryInjectionTest.kt
+++ app/src/test/java/com/omaestre/marvel/injection/RepositoryInjectionTest.kt
@@ -1,10 +1,17 @@
 package com.omaestre.marvel.injection
 
+import com.google.gson.Gson
 import com.omaestre.marvel.base.utils.Constants
+import com.omaestre.marvel.domain.model.ResultData
 import com.omaestre.marvel.network.MarvelService
 import com.omaestre.marvel.repository.HeroesRepository
+import com.omaestre.marvel.utils.MockValues
 import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
 import org.junit.Assert.assertNotNull
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.koin.test.KoinTest
@@ -16,6 +23,7 @@ class RepositoryInjectionTest : KoinTest {
 
     // Lazy inject property
     private val repository by inject<HeroesRepository>()
+    private lateinit var server : MockWebServer
 
     @get:Rule
     val koinTestRule = KoinTestRule.create {
@@ -24,24 +32,44 @@ class RepositoryInjectionTest : KoinTest {
         modules(repositoryModule)
     }
 
+
+    @Before
+    fun setUp(){
+        server = MockWebServer()
+        server.start()
+    }
+
     @Test
     fun injectService(): Unit = runBlocking {
         // directly request an instance
         assertNotNull(repository)
 
-        val repository = HeroesRepository(MarvelService(Constants.SERVICEURL))
+        // Schedule some responses.
+        server.enqueue(MockResponse().setResponseCode(200).setBody(MockValues.getMockJson()))
+
+        // Ask the server for its URL.
+        val baseUrl = server.url("/")
+
+
+        val repository = HeroesRepository(MarvelService(baseUrl.toString()))
         val response = repository.getHeroes()
-        val serviceResponse = repository.getMarvelService().getHeroes()
 
         assertNotNull(response.data)
         assertNotNull(response.data?.data)
+
+        val mockServiceResponse =
+            Gson().fromJson(MockValues.getMockJson(), ResultData::class.java) as ResultData
+
         assertTrue {
             response.data?.data?.results?.get(0)?.name.equals(
-                serviceResponse.data?.data?.results?.get(
-                    0
-                )?.name
+                mockServiceResponse.data.results[0].name
             )
         }
 
+    }
+
+    @After
+    fun tearDown() {
+        server.shutdown()
     }
 }
--- app/src/test/java/com/omaestre/marvel/injection/ServiceInjectionTest.kt
+++ app/src/test/java/com/omaestre/marvel/injection/ServiceInjectionTest.kt
@@ -1,42 +1,64 @@
 package com.omaestre.marvel.injection
 
+import com.google.gson.Gson
 import com.omaestre.marvel.base.utils.Constants
+import com.omaestre.marvel.domain.model.ResultData
 import com.omaestre.marvel.network.MarvelService
 import com.omaestre.marvel.repository.HeroesRepository
+import com.omaestre.marvel.utils.MockValues
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
 import org.junit.Assert.assertNotNull
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.koin.core.context.loadKoinModules
+import org.koin.dsl.module
 import org.koin.test.KoinTest
 import org.koin.test.KoinTestRule
 import org.koin.test.inject
+import org.koin.test.mock.MockProvider
+import org.koin.test.mock.MockProviderRule
 import kotlin.test.assertTrue
 
 class ServiceInjectionTest : KoinTest {
 
     // Lazy inject property
     private val service by inject<MarvelService>()
+    private lateinit var server : MockWebServer
 
     @get:Rule
     val koinTestRule = KoinTestRule.create {
-        printLogger()
-        modules(retrofitModule)
+        val mockedModule = module{
+            single{
+                MarvelService(server.url("/").toString())
+            }
+        }
+        loadKoinModules(mockedModule)
+    }
+
+    @Before
+    fun setUp(){
+        server = MockWebServer()
+        server.start()
     }
 
     @Test
     fun injectService() {
         // directly request an instance
         assertNotNull(service)
-
-        val repository = HeroesRepository(MarvelService(Constants.SERVICEURL))
+        // Schedule some responses.
+        server.enqueue(MockResponse().setResponseCode(200).setBody(MockValues.getMockJson()))
         val response = service.getHeroes()
+
+        val mockServiceResponse =
+            Gson().fromJson(MockValues.getMockJson(), ResultData::class.java) as ResultData
 
         assertNotNull(response.data)
         assertNotNull(response.data?.data)
         assertTrue {
             response.data?.data?.results?.get(0)?.name.equals(
-                repository.getMarvelService().getHeroes().data?.data?.results?.get(
-                    0
-                )?.name
+                mockServiceResponse.data.results[0].name
             )
         }
     }
